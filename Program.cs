68db027aef9818cbe79cb4d3b612343fcefd4078
minaiy.talati <minaiy.talati@NAG1-LHP-N07128>
2021-07-24 03:29:17 +0530

    update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace JenkDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Sum(2, 3).ToString());
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
