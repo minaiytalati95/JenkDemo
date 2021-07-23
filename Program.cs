4055005d0d5a40ebe02ceb67ab61dde688dd1472
minaiy.talati <minaiy.talati@NAG1-LHP-N07128>
2021-07-24 02:09:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
